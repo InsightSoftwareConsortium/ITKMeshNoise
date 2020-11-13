80d6b80b2fbe9155866fe65c4f988a39a362e71b
Mathew Seng <mathewseng@gmail.com>
2020-11-13 09:38:15 -0600

    ENH: Update supported Python versions for package builds This minimum version has been bumped from 3.5 to 3.6. The maximum has been bumped from 3.8 to 3.9. In order to enable this, the ITK version has been bumped to 5.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     keywords='ITK InsightToolkit Mesh Noise',
     url=r'https://github.com/InsightSoftwareConsortium/ITKMeshNoise',
     install_requires=[
-        r'itk'
+        r'itk>=5.1.1'
     ]
     )
